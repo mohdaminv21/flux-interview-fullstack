8246d52441df7ab348acea47d292f80deb60240c
mohd.amin <mohd.amin@thestar.com.my>
2020-10-11 22:18:30 +0800

    Do validation and save to pricing.json

--- pages/api/save-pricing.ts
+++ pages/api/save-pricing.ts
@@ -1,14 +1,38 @@
 import * as Joi from '@hapi/joi'
-
-
+import * as fs from 'fs';
 
 // This is the JOI validation schema, you define
 // all the validation logic in here, then run
 // the validation during the request lifecycle.
 // If you prefer to use your own way of validating the 
 // incoming data, you can use it.
-const schema = Joi.object<import('../../types').Matrix>({
+type MatrixType = import('../../types').Matrix;
+// const schema = Joi.object<import('../../types').Matrix>({
+//    matrix: Matrix
+// })
 
+// Build validation manually, cannot directly add type as joi object
+const schema = Joi.object().keys({
+  "36months": Joi.object({
+    lite: Joi.number(),
+    standard: Joi.number(),
+    unlimited: Joi.number()
+  }),
+  "24months": Joi.object({
+    lite: Joi.number(),
+    standard: Joi.number(),
+    unlimited: Joi.number()
+  }),
+  "12months": Joi.object({
+    lite: Joi.number(),
+    standard: Joi.number(),
+    unlimited: Joi.number()
+  }),
+  "mtm": Joi.object({
+    lite: Joi.number(),
+    standard: Joi.number(),
+    unlimited: Joi.number()
+  })
 })
 
 export default async (req: import('next').NextApiRequest, res: import('next').NextApiResponse) => {
@@ -16,22 +40,29 @@ export default async (req: import('next').NextApiRequest, res: import('next').Ne
     // This will throw when the validation fails
     const data = await schema.validateAsync(req.body, {
       abortEarly: false
-    }) as import('../../types').Matrix
+    }) //as import('../../types').Matrix
 
     // Write the new matrix to public/pricing.json
+    const strData = JSON.stringify(data, null, 2);
+
+    fs.writeFile('public/pricing.json', strData, (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    });
 
     res.statusCode = 200
     res.json(data)
   } catch(e) {
-    console.error(e)
     if(e.isJoi) {
+      console.log('joi error>>', e.details)
       // Handle the validation error and return a proper response
       res.statusCode = 422
-      res.end('Error')
-      return
+      // res.end('Error')
+      res.json({ error: e.details[0].message })
     }
-    
-    res.statusCode = 500
-    res.json({ error: 'Unknown Error' })
+    else {
+      res.statusCode = 500
+      res.json({ error: 'Unknown Error' })
+    }    
   }
 }
